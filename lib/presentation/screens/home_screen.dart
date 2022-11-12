9edfe2ede09a4f6e99f2ca10e10a055f4f0c2ec6
Ahmed Fadlalla <fadlallaahmed5@gmail.com>
2022-11-12 15:50:47 +0200

    kids_app

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kids_app/presentation/controller/Home/home_bloc.dart';
 import 'package:kids_app/presentation/controller/Home/home_state.dart';
 import 'package:kids_app/presentation/screens/component/main_home_screnn/audio_screen.dart';
+import 'package:video_player/video_player.dart';
 
 import '../../core/services/service_locator.dart';
 import '../controller/Home/home_event.dart';
@@ -17,81 +18,83 @@ class HomeScreen extends StatelessWidget {
   final String audioUrl;
   final String conversation;
   final String masrah;
-   HomeScreen({
-     Key? key,
-     required this.videoUrl,
-     required this.audioUrl,
-     required this.conversation,
-     required this.masrah,
-   }) : super(key: key);
 
-  List<Color>colors=[
-     Color(0xFFff008B),
-     Color(0xFF00e8ff),
-     Color(0xffae00ff),
-     Color(0xFF0068ff),
+  HomeScreen({
+    Key? key,
+    required this.videoUrl,
+    required this.audioUrl,
+    required this.conversation,
+    required this.masrah,
+  }) : super(key: key);
 
+  List<Color> colors = [
+    Color(0xFFff008B),
+    Color(0xFF00e8ff),
+    Color(0xffae00ff),
+    Color(0xFF0068ff),
+  ];
 
-
-   ];
-
-  List<String> texts=[
+  List<String> texts = [
     'اغنية',
     'فيديو',
     'مسرحية',
     'حوار',
-
   ];
 
-  List<String> lottie=[
+  List<String> lottie = [
     'assets/lottie/music1.json',
     'assets/lottie/video.json',
     'assets/lottie/masrah.json',
     'assets/lottie/conversation.json',
   ];
 
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (context)=>sl<HomeBloc>(),
-        child: BlocBuilder<HomeBloc,HomeState>(builder: (context,state){
-          return Scaffold(
-            appBar: AppBar(),
-            body:Container(
-              decoration: const BoxDecoration(
-
-                  gradient: LinearGradient(
-                      colors: [
-                        Color(0xFF0f17ad),
-                        Color(0xFF6985e8),
-                      ]
-                  )
-
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ListView.separated(
-                  shrinkWrap: true,
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context,index)=>ContainerDesignScreen(
+    return BlocProvider(
+      create: (context) => sl<HomeBloc>(),
+      child: BlocBuilder<HomeBloc, HomeState>(builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color(0xFF0f17ad),
+              Color(0xFF6985e8),
+            ])),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ListView.separated(
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) => ContainerDesignScreen(
                     lottieFile: lottie[index],
-                      color: colors[index],
-                      text: texts[index],
-                      widget: [
-                        AudioScreen(audio:audioUrl),
-                        VideoScreen(videoUrl:videoUrl,),
-                        MasrahyaScreen(video: masrah,),
-                        ConversationScreen(video: conversation),
-                      ][index]),
-                  separatorBuilder:(context,index)=>const SizedBox(
-                    height: 25,
-                  ) ,
-                  itemCount: texts.length,
+                    color: colors[index],
+                    text: texts[index],
+                    widget: [
+                      AudioScreen(url: audioUrl),
+                      AudioScreen(url: videoUrl),
+                      MasrahyaScreen(
+                        videoPlayerController: VideoPlayerController
+                            .network(
+                            masrah
+                        ),
+                      ),
+                      MasrahyaScreen(
+                        videoPlayerController: VideoPlayerController
+                            .network(
+                            conversation
+                        ),
+                      ),
+                    ][index]),
+                separatorBuilder: (context, index) => const SizedBox(
+                  height: 25,
                 ),
+                itemCount: texts.length,
               ),
-            ) ,
-          );
-    }),
+            ),
+          ),
+        );
+      }),
     );
   }
 }
